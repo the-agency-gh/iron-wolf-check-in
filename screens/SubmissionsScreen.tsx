cb05ec6d1070eed567a1c21ae7f268256fb3b5fa
Arthur Lee <arthur.lee945@gmail.com>
2023-03-28 22:19:14 -0500

    ud

--- screens/SubmissionsScreen.tsx
+++ screens/SubmissionsScreen.tsx
@@ -1,6 +1,18 @@
+import { StackScreenProps } from "@react-navigation/stack";
+import { FC, useLayoutEffect } from "react";
 import { StyleSheet, SafeAreaView, View, Text } from "react-native";
+import BackIcon from "../components/navigation/BackIcon";
 
-const SubmissionsScreen = () => {
+interface SubmissionsScreenProps extends StackScreenProps<any> {}
+
+const SubmissionsScreen: FC<SubmissionsScreenProps> = ({ navigation }) => {
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => {
+        return <BackIcon onPress={() => navigation.goBack()} />;
+      },
+    });
+  }, [navigation]);
   return (
     <SafeAreaView style={styles.screen}>
       <Text>SubmissionsScreen</Text>
