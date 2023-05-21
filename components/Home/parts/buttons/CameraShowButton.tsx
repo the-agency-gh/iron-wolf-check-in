0916a51ed0791870827129848a106416a175e5c3
Golden Tendon <arajchwald@gmail.com>
2023-05-21 11:49:59 -0500

    button styles

--- components/Home/parts/buttons/CameraShowButton.tsx
+++ components/Home/parts/buttons/CameraShowButton.tsx
@@ -1,7 +1,14 @@
 import { FC } from "react";
-import { Pressable, Text, ImageBackground, StyleSheet, StyleProp, View } from "react-native";
-import { colors, shadow } from "../../../../styles/variables";
+import {
+  ImageBackground,
+  Pressable,
+  StyleProp,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import CameraIcon from "../../../../assets/icons/camera-icon.svg";
+import { colors, shadow } from "../../../../styles/variables";
 
 interface CameraShowButtonProps {
   text: string;
@@ -10,12 +17,21 @@ interface CameraShowButtonProps {
   backgroundImg?: string | undefined;
 }
 
-const CameraShowButton: FC<CameraShowButtonProps> = ({ text, style, onPress, backgroundImg }) => {
+const CameraShowButton: FC<CameraShowButtonProps> = ({
+  text,
+  style,
+  onPress,
+  backgroundImg,
+}) => {
   return (
-    <Pressable onPress={onPress} style={[styles.button, shadow, style]} android_ripple={{ color: colors.baseBlack }}>
+    <Pressable
+      onPress={onPress}
+      style={[styles.button, shadow, style]}
+      android_ripple={{ color: colors.baseBlack }}
+    >
       {!backgroundImg ? (
         <View style={styles.indicatorCont}>
-          <CameraIcon />
+          <CameraIcon stroke={colors.white} />
         </View>
       ) : (
         <ImageBackground
@@ -24,7 +40,14 @@ const CameraShowButton: FC<CameraShowButtonProps> = ({ text, style, onPress, bac
           style={[styles.indicatorCont, { padding: 0, opacity: 0.7 }]}
         />
       )}
-      <Text style={[styles.text, { color: !backgroundImg ? colors.darkBlack : colors.darkBlue }]}>{text}</Text>
+      <Text
+        style={[
+          styles.text,
+          { color: !backgroundImg ? colors.white : colors.darkBlack },
+        ]}
+      >
+        {text}
+      </Text>
     </Pressable>
   );
 };
@@ -35,7 +58,9 @@ const styles = StyleSheet.create({
   button: {
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: colors.white,
+    backgroundColor: colors.darkBlack,
+    borderWidth: 5,
+    borderColor: colors.white,
     borderRadius: 20,
     width: "100%",
     height: "100%",
